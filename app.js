5379b3fba98b1c29c25547ef28df9b51ad6e02bd
Shahzil Sheikh <shazy792@hotmail.com>
2017-01-21 04:53:01 -0600

    Works!

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
 var express = require("express");
-var path = require("path");
+//var path = require("path");
 
 const PORT = process.env.port || process.env.PORT || 3000;
 
 var app = express();
 
-app.use(express.static(path.join(__dirname, './public')));
-
 app.get('/', (req, res) => {
-	res.sendFile("home.html");
+	res.sendFile(__dirname + "/home.html");
 }) 
 
 app.listen(PORT, (err) => {
