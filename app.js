54a57ee5f8f32298f120300ddd7733163a30f584
Shahzil Sheikh <shazy792@hotmail.com>
2017-01-21 04:41:19 -0600

    Relative Path Added

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 var express = require("express");
 //var path = require("path");
+process.env.PWD = process.cwd()
 
 const PORT = process.env.port || process.env.PORT || 3000;
 
 var app = express();
+
+app.use(express.static(process.env.PWD + '/static'));
 
 app.get('/', (req, res) => {
 	res.sendFile("./home.html");
