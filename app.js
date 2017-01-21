593cf311f6be43e25ec04b9daedd794ec2f82be1
Shahzil Sheikh <shazy792@hotmail.com>
2017-01-21 10:31:31 -0600

    Screenshot Done

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 var express = require("express");
-//var path = require("path");
+var depLinker = require("dep-linker");
 
 const PORT = process.env.port || process.env.PORT || 3000;
 
 var app = express();
 
+depLinker.linkDependenciesTo('./public')
+
 app.get('/', (req, res) => {
-	res.sendFile(__dirname + "/home.html");
+	res.sendFile(__dirname + "/public/home.html");
 }) 
 
 app.listen(PORT, (err) => {
