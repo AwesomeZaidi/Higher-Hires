40e059725d2c463eeed9f625cf0f340191ab84a4
Shahzil Sheikh <shazy792@hotmail.com>
2017-01-21 04:35:19 -0600

    Path added

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 var express = require("express");
+var path = require("path");
 
 const PORT = process.env.port || process.env.PORT || 3000;
 
 var app = express();
 
 app.get('/', (req, res) => {
-	res.sendFile("C:/Users/shazy/Desktop/DevelopmentProjects/boilermake/HigherHire/home.html");
+	console.log("Path>>", path.dirname());
+	res.sendFile("./home.html");
 }) 
 
 app.listen(PORT, (err) => {
