adf888d1da14ab5c5b9a3d2702cfb7e8a852dc0a
Shahzil Sheikh <shazy792@hotmail.com>
2017-01-21 04:46:15 -0600

    Path

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 var express = require("express");
-//var path = require("path");
-process.env.PWD = process.cwd()
+var path = require("path");
 
 const PORT = process.env.port || process.env.PORT || 3000;
 
 var app = express();
 
-app.use(express.static(process.env.PWD + '/static'));
+app.use(express.static(path.join(__dirname, './public')));
 
 app.get('/', (req, res) => {
 	res.sendFile("./home.html");
