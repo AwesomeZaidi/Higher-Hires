2f606f31e62ffbbf7a7d576a2310c5912e30d088
Asim Zaidi <asimzaidih@gmail.com>
2018-11-24 10:59:53 -0800

    MVC

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# HigherHire
+# HigherHires
+
+## We need a video player chat on the web. 
+
+## From there we can use Microsoft Cognitive Services Face API to detect emotion in video
+
+## Feed that result back to interviewee in real time and a report afterward.
--- server.js
+++ server.js
@@ -1,30 +1,37 @@
+
+const createError = require('http-errors');
+const path = require('path');
+const cookieParser = require('cookie-parser');
 const express = require("express");
 const bodyParser = require("body-parser");
-
 const PORT = process.env.port || process.env.PORT || 3000;
+// create these controller routers soon.
+var indexRouter = require('./controllers/index');
+// var usersRouter = require('./routes/users');
+const app = express();
 
-var app = express();
-
-// rename these!
-const points = [];
-const hr;
+// rename these, because i'm not sure what they even are!
+// const points = [];
+// const hr;
 
 app.use(bodyParser.urlencoded({ extended: false }))
  
 // parse application/json 
 app.use(bodyParser.json())
 
-app.get('/interviewer', (req, res) => {
-	res.render('interviewer');
-})
 
-app.get('/', (req, res) => {
-	res.render('/');
-})
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+const exphbs = require('express-handlebars');
 
-app.get('/interviewee', (req, res) => {
-	res.render('interviewee');
-})
+app.engine('hbs', exphbs({
+  defaultLayout: "main",
+  extname: ".hbs",
+  helpers: require("handlebars-helpers")()
+}));
+app.set('view engine', 'hbs');
+
+app.use('/', indexRouter);
 
 app.get('/plot', (req, res) => {
 	res.render('plot');
